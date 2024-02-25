0a83630527c931d44a29617f325edc083e441f30
Daria <English980@mail.ru>
2024-02-25 17:00:20 +0700

    task/009-remove-textview: text removed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.core.view.contains
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val textView by lazy { findViewById<TextView>(R.id.titleTextView) }
+    private val rootLayout: LinearLayout by lazy { findViewById(R.id.rootLayout) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState?.getBoolean(KEY_TEXT_REMOVED)?.let {
+            if (it) {
+                rootLayout.removeView(textView)
+            }
+        }
+
+        findViewById<Button>(R.id.removeButton).setOnClickListener {
+            rootLayout.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(KEY_TEXT_REMOVED, !rootLayout.contains(textView))
+        super.onSaveInstanceState(outState)
+    }
+
+    companion object {
+        private const val KEY_TEXT_REMOVED = "text.removed"
     }
 }
