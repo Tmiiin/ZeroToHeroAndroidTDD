311176a38ff1bdf989862da160dc3f5989e8821e
Daria <English980@mail.ru>
2024-02-26 02:09:50 +0700

    task/010-disable-button completed with OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,13 +1,16 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
-import android.widget.LinearLayout
-import androidx.core.view.contains
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private var state: State = State.Initial
 
     private val textView: TextView by lazy { findViewById(R.id.titleTextView) }
     private val rootLayout: LinearLayout by lazy { findViewById(R.id.rootLayout) }
@@ -16,25 +19,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        savedInstanceState?.getBoolean(KEY_TEXT_REMOVED)?.let {
-            if (it) {
-                rootLayout.removeView(textView)
-                button.isEnabled = false
-            }
-        }
 
         button.setOnClickListener {
-                rootLayout.removeView(textView)
-                it.isEnabled = false
-            }
+            state = State.Removed
+            state.apply(rootLayout, textView, button)
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean(KEY_TEXT_REMOVED, !rootLayout.contains(textView))
+        outState.putSerializable(KEY_STATE, state)
         super.onSaveInstanceState(outState)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY_STATE, State::class.java) as State
+        } else {
+            savedInstanceState.getSerializable(KEY_STATE) as State
+        }
+        state.apply(rootLayout, textView, button)
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
     companion object {
-        private const val KEY_TEXT_REMOVED = "text.removed"
+        private const val KEY_STATE = "text.removed"
+    }
+}
+
+sealed interface State : Serializable {
+
+    fun apply(linearLayout: LinearLayout, textView: TextView, button: Button)
+
+    data object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) = Unit
+    }
+
+    data object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) {
+            linearLayout.removeView(textView)
+            button.isEnabled = false
+        }
     }
 }
