65d90069080991b0d1eb7953c7d22a1ac50a0fa9
Daria <English980@mail.ru>
2024-04-16 23:56:05 +0700

    task/020-watch-input completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,28 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val textView: TextView by lazy { findViewById(R.id.titleTextView)}
+    private val button:Button by lazy { findViewById(R.id.actionButton) }
+    private val inputEditText: TextInputEditText by lazy { findViewById(R.id.inputEditText) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = inputEditText.text.toString()
+            inputEditText.text?.clear()
+        }
+        inputEditText.doOnTextChanged { text, _, _, _ ->
+            text ?: return@doOnTextChanged
+            button.isEnabled = text.length >= 3
+        }
     }
 }
