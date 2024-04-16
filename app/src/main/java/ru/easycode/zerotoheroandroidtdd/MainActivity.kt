9af1c594e8d2def09f10f1ddcc6c4a726b62d4a9
Daria <English980@mail.ru>
2024-04-16 23:35:08 +0700

    task/020-input completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val inputView: TextInputEditText by lazy { findViewById(R.id.inputEditText) }
+    private val textView: TextView by lazy { findViewById(R.id.titleTextView) }
+    private val button: Button by lazy { findViewById(R.id.actionButton) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = inputView.text.toString()
+            inputView.text?.clear()
+        }
     }
 }
